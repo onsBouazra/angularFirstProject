84a498e0aa68f0c18527b75dbe27f6ba6d226d4e
bouazra ons <ons986692@gmail.com>
2020-04-22 23:32:37 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+
 
 @NgModule({
   declarations: [
+   
+    ProductListComponent,
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/products/produit.ts
@@ -0,0 +1,23 @@
+export interface IProduit{
+    productId:number;
+    productName:string;
+    productCode:string;
+    releaseDate:string;
+    description:string;
+    price:number;
+    starRating:number;
+    imageUrl:string;
+    calculateDiscount(percent:number):number;
+}
+//export class Produit implements IProduit{
+ //   constructor(public productId:number,
+   //     public productName:string,
+     //   public productCode:string,
+       // public releaseDate:string,
+   //     public description:string,
+     //   public price:number,
+       // public starRating:number,
+//        public imageUrl:string){}
+  //  calculateDiscount(percent:number):number{
+   //   return this.price-(this.price*percent/100);
+   // }}
